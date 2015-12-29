edb910e44fb31451651535da553a83e60437800d
abirjepatil <abhishek_birjepatil@hotmail.com>
2015-12-29 13:57:00 -0800

    Variables
    Learning the basics:- Variables, Constants, Arrays and Control
    Statements

---
+++ Playground/HelloWorld.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+
+//See how variables and constants are declared
+
+var count = 10
+print(count)
+
+//Declare count as an integer variable
+var countExplicit:Int = 50
+print(countExplicit)
+
+
+//Loop through an integer array
+var arrayInt = [10,12,12,44]
+
+for value in arrayInt
+{
+    if value < 44
+    {
+        print(value)
+    }
+}
