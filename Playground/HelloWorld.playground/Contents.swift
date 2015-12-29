8281f29d9cf3eba0b59a2ba80e2a68fb6a2cb3f5
abirjepatil <abhishek_birjepatil@hotmail.com>
2015-12-29 14:18:51 -0800

    Classes And Object Oriented Concepts

--- Playground/HelloWorld.playground/Contents.swift
+++ Playground/HelloWorld.playground/Contents.swift
@@ -26,3 +26,51 @@ for value in arrayInt
         print(value)
     }
 }
+
+//Switch Case in Apple Swift
+let option =  "CASES"
+
+switch option{
+    case "CASES":
+        print("Will enter here");
+    case "NOCASEMATCH":
+        print("Will Not Enter Here")
+default:
+    print("Nothing");
+}
+/*A switch case in swift requires the default clause*/
+
+
+/*Hands on with Functions*/
+
+//func keyword is used to declare functions
+// Arguments to the function as variable name : variable type
+// return type after the arguments
+func Message(message: String) ->String
+{
+    return "Hello \(message)"
+}
+
+
+//Lets call the function
+Message("Hello")
+
+
+//Function to find the factorial of a number
+func FindFactorial(number:Int) ->Int
+{
+    var count = number
+    var result = 1 ;
+    while(count>0)
+    {
+        result=result*count;
+        count=count-1;
+    }
+    
+    return result;
+}
+
+//Call the Factorial Functions
+FindFactorial(5)
+
+
